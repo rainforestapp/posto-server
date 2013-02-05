e5ccf194960bbea7d52f24e052e27d36d7323d49
Greg Fodor <gfodor@etsy.com>
2013-02-05 01:47:57 -0800

    try again

--- config/initializers/set_schema_search_path.rb
+++ config/initializers/set_schema_search_path.rb
@@ -1,1 +1,3 @@
-ActiveRecord::Base.connection.schema_search_path = "posto0" rescue nil
+if ActiveRecord::Base.connection.raw_connection.host
+  ActiveRecord::Base.connection.schema_search_path = "posto0"
+end
