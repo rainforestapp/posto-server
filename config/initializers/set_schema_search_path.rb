121613b468c4be0bb208231da6c3a5b0b6f807c3
Greg Fodor <gfodor@etsy.com>
2013-02-05 02:01:23 -0800

    Hopefully fix deploy

--- config/initializers/set_schema_search_path.rb
+++ config/initializers/set_schema_search_path.rb
@@ -1,4 +1,4 @@
-begin
+if ActiveRecord::Base.configurations[Rails.env]["host"] &&
+   ActiveRecord::Base.configurations[Rails.env]["host"] != "127.0.0.1"
   ActiveRecord::Base.connection.schema_search_path = "posto0"
-rescue nil
 end
