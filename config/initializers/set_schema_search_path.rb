e514fce87874ab17b98ee429291c17fddbab04e1
Greg Fodor <gfodor@etsy.com>
2013-02-05 02:03:45 -0800

    Arg

--- config/initializers/set_schema_search_path.rb
+++ config/initializers/set_schema_search_path.rb
@@ -1,5 +1,6 @@
 if ActiveRecord::Base.configurations
    ActiveRecord::Base.configurations[Rails.env] &&
    ActiveRecord::Base.configurations[Rails.env]["host"] != "127.0.0.1"
-  ActiveRecord::Base.connection.schema_search_path = "posto0"
+   puts ActiveRecord::Base.configurations.inspect
+  #ActiveRecord::Base.connection.schema_search_path = "posto0"
 end
