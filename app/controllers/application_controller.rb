8534c0ac59934feca4b2d0afa1bddce4d9df8058
Greg Fodor <gfodor@etsy.com>
2013-02-05 01:43:22 -0800

    Perhaps fix for search path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_pgsql_search_path
+
+  def set_pgsql_search_path
+    ActiveRecord::Base.connection.schema_search_path = "posto0"
+  end
 end
