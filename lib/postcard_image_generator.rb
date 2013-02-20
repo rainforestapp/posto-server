f4337349821b32fdfbbd973aaa7923ee0d7350b1
Greg Fodor <gfodor@etsy.com>
2013-02-20 02:01:28 -0800

    Cleanup memory freeing

--- lib/postcard_image_generator.rb
+++ lib/postcard_image_generator.rb
@@ -25,81 +25,88 @@ class PostcardImageGenerator
 
     composed_image_url = card_design.composed_full_photo_image.public_url
 
-    with_file_for_url(composed_image_url) do |composite_file|
+    with_image_for_url(composed_image_url) do |composite|
       profile_image_url = "https://graph.facebook.com/#{sender_user.facebook_id}/picture?width=200&height=200"
 
-      with_file_for_url(profile_image_url) do |profile_image_file|
+      with_image_for_url(profile_image_url) do |fb_profile|
+        fb_profile.resize_to_fill!(150,150)
+
         with_closed_tempfile do |back_qr_file|
           with_closed_tempfile do |front_qr_file|
             with_closed_tempfile do |front_image_file|
               with_closed_tempfile do |back_image_file|
-                composite = Magick::Image.read(composite_file.path).first
-                cols = composite.columns
-                rows = composite.rows
+                with_image(template_path + "/FrontTemplate#{title_on_top ? "Flipped" : ""}.png") do |front_template|
+                  with_image(template_path + "/BackTemplate.png") do |back|
+                    cols = composite.columns
+                    rows = composite.rows
 
-                border_pixels = (BORDER_SIZE * [cols, rows].max.to_f).floor
+                    border_pixels = (BORDER_SIZE * [cols, rows].max.to_f).floor
 
-                front_template = Magick::Image.read(template_path + "/FrontTemplate#{title_on_top ? "Flipped" : ""}.png").first
-                back = Magick::Image.read(template_path + "/BackTemplate.png").first
+                    front_qr = RQRCode::QRCode.new(URI.escape(QR_PATH + front_scan_key), size: 4, level: :h)
+                    front_qr_png = front_qr.to_img.resize(175,175).save(front_qr_file.path)
+                    back_qr = RQRCode::QRCode.new(URI.escape(QR_PATH + back_scan_key), size: 4, level: :h)
+                    back_qr_png = back_qr.to_img.resize(175,175).save(back_qr_file.path)
 
-                front_qr = RQRCode::QRCode.new(URI.escape(QR_PATH + front_scan_key), size: 4, level: :h)
-                front_qr_png = front_qr.to_img.resize(175,175).save(front_qr_file.path)
-                front_qr_image = Magick::Image.read(front_qr_file.path).first
-                front_template.composite!(front_qr_image, 1569, 97, Magick::DstOverCompositeOp)
+                    with_image(front_qr_file.path) do |front_qr_image|
+                      with_image(back_qr_file.path) do |back_qr_image|
+                        front_template.composite!(front_qr_image, 1569, 97, Magick::DstOverCompositeOp)
 
-                front = composite.resize_to_fill(front_template.rows, front_template.columns)
-                front.rotate!(title_on_top ? 90 : 270)
-                wallet_card = composite.rotate(270)
-                wallet_card.resize_to_fill!(1050, 750)
-                front.composite!(front_template, 0, 0, Magick::OverCompositeOp)
+                        front = composite.resize_to_fill(front_template.rows, front_template.columns)
 
-                back_qr = RQRCode::QRCode.new(URI.escape(QR_PATH + back_scan_key), size: 4, level: :h)
-                back_qr_png = back_qr.to_img.resize(175,175).save(back_qr_file.path)
-                back_qr_image = Magick::Image.read(back_qr_file.path).first
+                        begin
+                          front.rotate!(title_on_top ? 90 : 270)
+                          wallet_card = composite.rotate(270)
+                          wallet_card.resize_to_fill!(1050, 750)
+                          front.composite!(front_template, 0, 0, Magick::OverCompositeOp)
 
-                fb_profile = Magick::Image.read(profile_image_file.path).first
-                fb_profile.resize_to_fill!(150,150)
+                          back.composite!(wallet_card, 20, 20, Magick::DstOverCompositeOp)
+                          back.composite!(back_qr_image, 94, 811, Magick::DstOverCompositeOp)
+                          back.composite!(fb_profile, 1135, 576, Magick::OverCompositeOp)
 
-                back.composite!(wallet_card, 20, 20, Magick::DstOverCompositeOp)
-                back.composite!(back_qr_image, 94, 811, Magick::DstOverCompositeOp)
-                back.composite!(fb_profile, 1135, 576, Magick::OverCompositeOp)
+                          back_with_text = Magick::Draw.new
 
-                back_with_text = Magick::Draw.new
+                          use_big_font = fb_name.size <= 20
+                          name_y_offset = use_big_font ? 620 : 610
+                          sent_y_offset = use_big_font ? 660 : 650
 
-                use_big_font = fb_name.size <= 20
-                name_y_offset = use_big_font ? 620 : 610
-                sent_y_offset = use_big_font ? 660 : 650
+                          back_with_text.annotate(back, 0, 0, 1310, name_y_offset, fb_name) do
+                            self.fill = "#FFFFFF"
+                            self.stroke = 'transparent'
+                            self.pointsize = use_big_font ? 48 : 32
+                            self.font_family = "HelveticaNeueL"
+                            self.text_align(Magick::LeftAlign)
+                          end
 
-                back_with_text.annotate(back, 0, 0, 1310, name_y_offset, fb_name) do
-                  self.fill = "#FFFFFF"
-                  self.stroke = 'transparent'
-                  self.pointsize = use_big_font ? 48 : 32
-                  self.font_family = "HelveticaNeueL"
-                  self.text_align(Magick::LeftAlign)
+                          back_with_text.annotate(back, 0, 0, 1310, sent_y_offset, sent_text) do
+                            self.fill = "#FC933E"
+                            self.stroke = 'transparent'
+                            self.pointsize = 28
+                            self.font_family = "HelveticaNeueL"
+                            self.text_align(Magick::LeftAlign)
+                          end
+
+                          front.write("png:" + front_image_file.path)
+                          back.write("png:" + back_image_file.path)
+
+                          with_image(front_image_file.path) do |front_pdf|
+                            with_image(back_image_file.path) do |back_pdf|
+                              [front_pdf, back_pdf].each do |img|
+                                img.add_profile("resources/icc/sRGB_v4_ICC_preference.icc")
+                                img.add_profile("resources/icc/USWebUncoated.icc")
+                                img.colorspace = Magick::CMYKColorspace
+                              end
+
+                              front_pdf.write("pdf:#{output_front_image_path}")
+                              back_pdf.write("pdf:#{output_back_image_path}")
+                            end
+                          end
+                        ensure
+                          front.try(:destroy!)
+                        end
+                      end
+                    end
+                  end
                 end
-
-                back_with_text.annotate(back, 0, 0, 1310, sent_y_offset, sent_text) do
-                  self.fill = "#FC933E"
-                  self.stroke = 'transparent'
-                  self.pointsize = 28
-                  self.font_family = "HelveticaNeueL"
-                  self.text_align(Magick::LeftAlign)
-                end
-
-                front.write("png:" + front_image_file.path)
-                back.write("png:" + back_image_file.path)
-
-                front_pdf = Magick::Image.read(front_image_file.path).first
-                back_pdf = Magick::Image.read(back_image_file.path).first
-
-                [front_pdf, back_pdf].each do |img|
-                  img.add_profile("resources/icc/sRGB_v4_ICC_preference.icc")
-                  img.add_profile("resources/icc/USWebUncoated.icc")
-                  img.colorspace = Magick::CMYKColorspace
-                end
-
-                front_pdf.write("pdf:#{output_front_image_path}")
-                back_pdf.write("pdf:#{output_back_image_path}")
               end
             end
           end
@@ -131,6 +138,25 @@ class PostcardImageGenerator
     Tempfile.open(SecureRandom.hex) do |file|
       file.binmode
       yield(file)
+    end
+  end
+
+  def with_image_for_url(url)
+    with_file_for_url(url) do |file|
+      with_image(file.path) do |image|
+        yield(image)
+      end
+    end
+  end
+
+  def with_image(path)
+    image = nil
+
+    begin 
+      image = Magick::Image.read(path).first
+      yield(image)
+    ensure
+      image.try(:destroy!)
     end
   end
 end
