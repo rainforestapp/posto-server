4d62fb06e82cffad8e59287db4758b21803cf49d
Greg Fodor <gfodor@gmail.com>
2013-02-21 16:42:41 -0800

    Push to SQS queues for manual tasks

--- lib/worker/manual_activities.rb
+++ lib/worker/manual_activities.rb
@@ -5,11 +5,21 @@ class ManualActivities
     true
   end
 
+  def queue_prefix
+    Rails.env == "development" ? "posto-dev" : "posto-prod"
+  end
+
   def try_manual_parsing_address_for_request(address_request_id)
-    puts "try parsing #{address_request_id} #{self.task_token}"
+    queue_name = "#{queue_prefix}-manual-address-parse"
+    payload = { address_request_id: address_request_id, task_token: self.task_token }
+    AWS::SQS.new.queues.named(queue_name).send_message(payload.to_json)
+    true
   end
 
   def manually_verify_order(card_order_id)
-    puts "manual verify #{card_order_id} #{self.task_token}"
+    queue_name = "#{queue_prefix}-manual-verify-order"
+    payload = { card_order_id: card_order_id, task_token: self.task_token }
+    AWS::SQS.new.queues.named(queue_name).send_message(payload.to_json)
+    true
   end
 end
