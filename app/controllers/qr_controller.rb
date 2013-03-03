a81d79d001e57a885dbea3563efccb39676816cd
Greg Fodor <gfodor@gmail.com>
2013-03-03 15:15:19 -0800

    QR controller

--- app/controllers/qr_controller.rb
+++ app/controllers/qr_controller.rb
@@ -13,8 +13,23 @@ class QrController < ApplicationController
       raise "Bad id #{params[:id]}"
     end
 
-    printing = CardPrinting.where(uid: uid).first
+    card_printing = CardPrinting.where(uid: uid).first
+    card_order = card_printing.card_order
+    card_design = card_order.card_design
+    preview_composition = card_design.card_preview_composition
 
+    out = {}
+    out[:sender_name] = card_order.order_sender_user.user_profile.name
+    out[:sent_on] = card_order.created_at.strftime("%-m/%-d/%y")
+    out[:treated_preview_image_url] = preview_composition.treated_card_preview_image.public_url
+    out[:preview_image_url] = preview_composition.card_preview_image.public_url
+    out[:original_full_photo_url] = card_design.original_full_photo_image.public_url
+
+    if card_design.edited_full_photo_image
+      out[:edited_full_photo_url] = card_design.edited_full_photo_image.public_url
+    end
+
+    render json: out
   end
 end
 
