ad5399fe85efa6055f10e983439de59031fdc29b
Greg Fodor <gfodor@gmail.com>
2013-03-07 15:19:16 -0800

    Nothing ever works

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,8 @@ Posto::Application.configure do
 
   if File.exists?(memcached_config_file)
     File.open(memcached_config_file) do |f|
-      config.cache_store = [:dalli_store] + YAML.load(f.read)["production"]["servers"]
+      cache_servers = YAML.load(f.read)[Rails.env]["servers"]
+      config.cache_store = [:dalli_store] + cache_servers if cache_servers
     end
   end
 
