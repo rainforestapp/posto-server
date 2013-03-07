5d1d43f0096df8f36acff09e8c510afd89ad7da8
Greg Fodor <gfodor@gmail.com>
2013-03-07 15:24:30 -0800

    Wrong place

--- deploy/before_migrate.rb
+++
@@ -1,7 +0,0 @@
-current_release = release_path
- 
-execute "rake assets:precompile" do
-  cwd current_release
-  command "bundle exec rake assets:precompile"
-  environment "RAILS_ENV" => "production"
-end
