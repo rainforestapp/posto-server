38d1faafe58f7d5fa942b37d041718dbd9e9bf99
Greg Fodor <gfodor@gmail.com>
2013-03-23 13:51:42 -0700

    Remove slashes

--- config/initializers/statsd.rb
+++ config/initializers/statsd.rb
@@ -4,7 +4,7 @@ STATSD.namespace = "posto"
 hostname = `hostname`.chomp
 
 ActiveSupport::Notifications.subscribe "process_action.action_controller" do |name, start, finish, id, payload|
-  path = "#{payload[:controller].gsub(/Controller$/, "").underscore}.#{payload[:action]}"
+  path = "#{payload[:controller].gsub(/Api::V.::/, "").gsub(/Controller$/, "").underscore}.#{payload[:action]}"
   total_time = ((finish - start) * 1000).floor
   view_time = payload[:view_runtime].floor
   db_time = payload[:db_runtime].floor
