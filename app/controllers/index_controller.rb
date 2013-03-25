94d877d91e2ebd27028efdcd9ad4dc078827adf1
Greg Fodor <gfodor@gmail.com>
2013-03-25 11:24:53 -0700

    Redirect to homepage

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,7 +2,6 @@ class IndexController < ApplicationController
   def show
     #expires_in 1.hour, public: true if Rails.env == "production"
 
-    render
-    #redirect_to "http://www.lulcards.com"
+    redirect_to "http://www.lulcards.com"
   end
 end
