0d1c2e297adfb2f1b945a741fa4beb974fe46954
Greg Fodor <gfodor@gmail.com>
2013-04-10 17:58:14 -0700

    Try negative processing trick

--- config/app/public.rb
+++ config/app/public.rb
@@ -42,8 +42,8 @@ CONFIG = SampleableConfig.define do
   fb_permissions ["email", "read_mailbox", "xmpp_login", "user_location", "friends_location", "user_photos", "friends_photos"]
   fb_message_permissions ["read_mailbox", "xmpp_login"]
   fb_fields ["gender", "birthday"]
-  processing_fee 1
-  card_fee 124
+  processing_fee -1
+  card_fee 125
   processing_credits 0
   card_credits 10
   referral_credits 5
