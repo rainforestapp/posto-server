e8f63eb3d3fdbf695e9312a341321d7e3d8ebb08
Greg Fodor <gfodor@gmail.com>
2013-04-11 21:45:32 -0700

    babycards config

--- app/models/app.rb
+++ app/models/app.rb
@@ -6,8 +6,12 @@ class App < ActiveRecord::Base
     @lulcards ||= App.where(name: "lulcards", apple_app_id: "585112745", domain: "lulcards.com").first_or_create!
   end
 
+  def self.babycards
+    @babycards ||= App.where(name: "babycards", apple_app_id: "634710276", domain: "sendbabycards.com").first_or_create!
+  end
+
   def self.by_name(name)
-    raise "Bad app name" unless name == "lulcards"
-    self.lulcards
+    raise "Bad app name" unless name == "lulcards" || name == "babycards"
+    name == "lulcards" ? self.lulcards : self.babycards
   end
 end
