43fc5ea290193149d51d5c8be41f466c43760e9d
Greg Fodor <gfodor@gmail.com>
2013-04-23 02:13:57 -0700

    Config update for birthdays

--- config/app/public.rb
+++ config/app/public.rb
@@ -212,10 +212,10 @@ CONFIG = SampleableConfig.define do
 
   facebook_birthday_allow_messages do
     variant 1, "hey setting up", {
-      header_primary: "We need to verify\n@@@'s birthday.",
-      header_secondary: "Send ### a Facebook message.",
+      header_primary: "@@@'s birthday\nisn't on Facebook.",
+      header_secondary: "Send ### a message to ask.",
       message: "hey when's your birthday? i'm setting up some reminders\n",
-      disclaimer: "This message will be sent if you set a birthday reminder for $$$. We'll notify you to confirm your reminder when %%% responds.",
+      disclaimer: "This message will be sent if you set a birthday reminder for @@@. We'll notify you to confirm your reminder when %%% responds.",
       title: "!!!",
       button_label: "Send",
       known_address_label: "I know @@@'s birthday",
@@ -224,7 +224,7 @@ CONFIG = SampleableConfig.define do
 
   birthday_reminder_recipient_prompt "Choose who you'll send birthday cards to."
   birthday_reminder_days 7
-  birthday_reminder_buy_info_text "You'll get a reminder 7 days before their birthdays."
+  birthday_reminder_buy_info_text "You'll get a reminder 7 days before their birthday."
 
   default_captions do
     variant 1, "many", [
