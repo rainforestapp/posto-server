5294960812fd2f1fc8ee39ff8730a26f1503fc91
Greg Fodor <gfodor@gmail.com>
2013-05-11 20:57:10 -0700

    Gift credit packages coming together

---
+++ app/assets/javascripts/gift_credits.js.coffee
@@ -0,0 +1,34 @@
+class GiftCreditsController
+  show_lookup_error: (error) ->
+    $("#lookup-controls").addClass("info");
+    $("#lookup-code-error").text(error)
+    
+  clear_lookup_error: ->
+    $("#lookup-controls").removeClass("info");
+    $("#lookup-code-error").text("")
+
+  init: ->
+    self = this
+
+    $("#lookup").click ->
+      lookup_code = $("#lookup-code").val()
+
+      return if lookup_code == ""
+
+      unless lookup_code.match(/^[0-9]+$/)
+        self.show_lookup_error("Your code should be all numbers.")
+        return
+
+      app_name = $("body").attr("data-app")
+
+      self.clear_lookup_error()
+
+      $.ajax "/apps/#{app_name}/gift_credits/#{lookup_code}.json", dataType: "json", success: (data) ->
+        if data.card_printing_id?
+          document.location.href = "/apps/#{app_name}/gift_credits/#{lookup_code}"
+        else
+          self.show_lookup_error("We couldn't find your card. Please double check your code.")
+
+      false
+
+window.Posto.gift_credits = GiftCreditsController
--- app/controllers/gift_credits_controller.rb
+++ app/controllers/gift_credits_controller.rb
@@ -1,9 +1,50 @@
 class GiftCreditsController < ApplicationController
+  layout "gift"
+
   def index
-    render text: "HI"
+    @app = App.by_name(params[:app_id])
+    @title = "babygrams: enter your babygram code"
   end
 
   def show
+    @app = App.by_name(params[:app_id])
+
+    @config = CONFIG.for_app(@app)
+    @card_printing = CardPrinting.find_by_lookup_number(params[:id].to_i)
+
+    if @card_printing
+      @card_order = @card_printing.card_order
+      @sender = @card_order.order_sender_user
+      @sender_profile = @sender.user_profile
+      @title = "babygrams: Buy credits for #{@sender_name}"
+      @postcard_subject = @card_order.card_design.postcard_subject
+      @postcard_subject_first_name = @postcard_subject[:name].split(" ").first
+      @recipient_name = @card_printing.recipient_user.try(:user_profile).try(:name) || ""
+
+      @credits = @sender.credits_for_app(@app)
+
+      @number_of_cards = @credits / @config.card_credits
+
+      if @number_of_cards <= 0
+        @credit_message = "#{@sender} is all out of credits."
+      else
+        @credit_message = "#{@sender_profile.subject_pronoun.capitalize} currently has #{@credits} credits, enough to send #{@number_of_cards} more #{"card".pluralize(@number_of_cards)}."
+      end
+    end
+
+    respond_to do |format|
+      format.json do 
+        if @card_printing
+          render json: { card_printing_id: @card_printing.card_printing_id }
+        else
+          render json: { }
+        end
+      end
+
+      format.html do
+        render
+      end
+    end
   end
 
   def create
