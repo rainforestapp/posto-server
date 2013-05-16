78168ae9926df8e6228a0a9f61c375a107bbb95d
Greg Fodor <gfodor@gmail.com>
2013-05-15 20:55:35 -0700

    Fix for index controller (thanks dad)

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -4,6 +4,8 @@ class IndexController < ApplicationController
 
     if request.host == "sendmebabygrams.com"
       redirect_to "https://secure.babygra.ms/apps/babygrams/gift_credits"
+    elsif request.host == "babygramsapp.com"
+      redirect_to "http://babygra.ms"
     else
       redirect_to "http://www.lulcards.com"
     end
