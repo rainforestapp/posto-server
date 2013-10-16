d448ac54d13a1cedf9ce8c0c447e1f903247811e
Greg Fodor <gfodor@gmail.com>
2013-10-15 20:36:15 -0700

    Better metadata

--- app/controllers/card_printings_controller.rb
+++ app/controllers/card_printings_controller.rb
@@ -7,6 +7,8 @@ class CardPrintingsController < ApplicationController
     @card_order = @card_printing.card_order
     @sender = @card_order.order_sender_user
     @sender_name = @sender.user_profile.name
+    @sender_possessive = @sender.user_profile.possessive_pronoun
+    @sender_first_name = @sender.user_profile.first_name
     @card_design = @card_order.card_design
     @message = @card_design.note if @card_design.note && @card_design.note.size > 0
     @app = @card_order.app
@@ -19,6 +21,7 @@ class CardPrintingsController < ApplicationController
     @meta_url = "http://#{@app.domain}/card_printings/#{@card_printing.uid}"
     @og_type = @config.open_graph_type
     @is_mobile = self.mobile_agent?
+    @location = ""
 
     @theme_color = "white" if @app == App.babygrams
     @sent_by_caption = "Sent by #{@sender.user_profile.name} with #{@app.name}"
@@ -39,8 +42,11 @@ class CardPrintingsController < ApplicationController
         short_baby_name = " of #{postcard_subject[:name].split(/\s+/)[0]}"
       end
 
-      @title = "#{@full_text} postcard#{baby_name} - made with #{@app.name}".strip
-      @og_title = "#{@full_text} postcard#{baby_name}".strip
+      @description = "#{@sender_first_name} designed, printed & mailed this photo#{baby_name} to someone special from #{@sender_possessive} iPhone with #{@app.name}."
+
+      @title = "#{@app.name.capitalize} - Photo postcard#{baby_name}".strip
+      @og_title = "#{@app.name.capitalize} photo postcard#{baby_name}"
+
       @sent_by_caption = "#{@sender.user_profile.first_name} mailed this printed photo#{short_baby_name} from #{@sender.user_profile.possessive_pronoun} iPhone with #{@app.name}."
     else
       @title = @config.page_title
